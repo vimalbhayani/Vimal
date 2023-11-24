158707227b5aeffa75871a2da8e7cc593cb68e2c
vimalbhayani <vimalpatel6353@gmail.com>
2023-11-24 12:08:01 +0530

    done

--- module 1/21d.cpp
+++ module 1/21d.cpp
@@ -4,11 +4,11 @@ int main() {
     int rows = 5;
 
     for (int i = 1; i <= rows; i++) {
-        for (int space = 1; space <= rows - i; space++) {
+        for (int i = 1; i <= rows - i; i++) {
             printf(" ");
         }
         for (int j = 1; j <= i; j++) {
-            printf("*");
+            printf(" *");
         }
         printf("\n");
     }
